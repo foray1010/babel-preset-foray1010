d86899e005cde00541251d193c1dce70df316217
foray1010 <foray1010@users.noreply.github.com>
2016-09-21 23:42:58 +0800

    Fix pointing to wrong path for babel-plugin-transform-react-remove-prop-types

--- react.js
+++ react.js
@@ -16,7 +16,7 @@ module.exports = {
       plugins: [
         require('babel-plugin-transform-react-constant-elements'),
         require('babel-plugin-transform-react-inline-elements'),
-        require('babel-plugin-transform-react-remove-prop-types')
+        require('babel-plugin-transform-react-remove-prop-types').default
       ]
     },
     test: {
