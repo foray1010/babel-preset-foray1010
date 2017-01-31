749a789a44e0bf0e93d17b2c3065f856290b3e08
foray1010 <foray1010@users.noreply.github.com>
2017-01-31 20:17:01 +0800

    Fix native modules && use string instead of require to require plugin

--- react.js
+++ react.js
@@ -12,31 +12,31 @@ const modifiedReactPreset = modifyBabelPreset('babel-preset-react', {
 
 module.exports = mergeByEnv({
   presets: [
-    [require('babel-preset-es2015'), {modules: false}],
-    require('babel-preset-es2016'),
-    require('babel-preset-es2017'),
+    ['babel-preset-es2015', {modules: false}],
+    'babel-preset-es2016',
+    'babel-preset-es2017',
     modifiedReactPreset
   ],
   plugins: [
-    require('babel-plugin-transform-decorators-legacy').default,
-    require('babel-plugin-transform-object-rest-spread')
+    'babel-plugin-transform-decorators-legacy',
+    'babel-plugin-transform-object-rest-spread'
   ],
   env: {
     production: {
       plugins: [
-        require('babel-plugin-transform-react-constant-elements'),
-        require('babel-plugin-transform-react-inline-elements'),
-        require('babel-plugin-transform-react-remove-prop-types').default
+        'babel-plugin-transform-react-constant-elements',
+        'babel-plugin-transform-react-inline-elements',
+        'babel-plugin-transform-react-remove-prop-types'
       ]
     },
     test: {
       plugins: [
-        require('babel-plugin-istanbul')
+        'babel-plugin-istanbul'
       ]
     },
     development: {
       plugins: [
-        require('babel-plugin-tcomb').default
+        'babel-plugin-tcomb'
       ]
     }
   }
