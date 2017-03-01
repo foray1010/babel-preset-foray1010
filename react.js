a682950f02d57414547a5b8fedc97f5d32f3aad4
foray1010 <foray1010@users.noreply.github.com>
2017-03-01 22:55:21 +0800

    Read browserslist config from app root directory

--- react.js
+++ react.js
@@ -1,20 +1,43 @@
 'use strict'
 
-const mergeAndConcat = require('merge-and-concat')
+const browserslistConfigByEnv = require('./lib/browserslistConfigByEnv')
+const mergeByEnv = require('./lib/mergeByEnv')
 
-const reactRules = require('./rules/react')
-
-module.exports = mergeAndConcat({
+module.exports = mergeByEnv({
   presets: [
     ['babel-preset-env', {
       modules: false,
       targets: {
-        browsers: [
-          '> 1%',
-          'last 2 versions'
-        ]
+        browsers: browserslistConfigByEnv
       },
       useBuiltIns: true
+    }],
+    'babel-preset-react'
+  ],
+  plugins: [
+    'babel-plugin-react-css-modules',
+    'babel-plugin-transform-decorators-legacy',
+    'babel-plugin-transform-object-rest-spread',
+    ['babel-plugin-transform-react-jsx', {
+      pragma: 'createElement'
     }]
-  ]
-}, reactRules)
+  ],
+  env: {
+    production: {
+      plugins: [
+        'babel-plugin-transform-react-constant-elements',
+        'babel-plugin-transform-react-remove-prop-types'
+      ]
+    },
+    test: {
+      plugins: [
+        'babel-plugin-istanbul'
+      ]
+    },
+    development: {
+      plugins: [
+        'babel-plugin-tcomb'
+      ]
+    }
+  }
+})
