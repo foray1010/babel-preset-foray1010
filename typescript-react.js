ceaa9b2b2df267aff1cb1c926e47a3207d7a7924
foray1010 <foray1010@users.noreply.github.com>
2019-01-21 20:59:20 +0800

    feat: support typescript with react

---
+++ typescript-react.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const getEnv = require('./lib/getEnv')
+
+module.exports = () => ({
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        modules: getEnv() === 'test' ? 'commonjs' : false,
+        useBuiltIns: 'entry'
+      }
+    ],
+    ['@babel/preset-react', {useBuiltIns: true}],
+    '@babel/preset-typescript'
+  ],
+  plugins: [
+    '@babel/plugin-proposal-class-properties',
+    ...(getEnv() === 'production' ?
+      [
+        '@babel/plugin-transform-react-constant-elements',
+        '@babel/plugin-transform-react-inline-elements',
+        ['babel-plugin-transform-react-remove-prop-types', {removeImport: true}]
+      ] :
+      [])
+  ]
+})
